58137067bc647c94dde48b06db5655ea8611a458
Ryan Eaton <ryaneaton@spea.cc>
2025-11-11 08:09:25 -0500

    init

---
+++ src/client/java/cc/spea/selectedblockhighlighter/SelectedBlockHighlighterClient.java
@@ -0,0 +1,10 @@
+package cc.spea.selectedblockhighlighter;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SelectedBlockHighlighterClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
